9acbda4b011cedd4d7b93fe2162d246efbcc7241
3kezoh <3kezoh@gmail.com>
2022-11-26 17:12:51 +0100

    refactor: update the socket io server architecture

--- server/src/types/socket.io.ts
+++ server/src/types/socket.io.ts
@@ -1,39 +1,60 @@
-import { Socket as IOSocket } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { IConversation } from "./conversation";
 import { IFullMessage } from "./message";
 import { IRoom } from "./room";
 
-export interface EmitEvents {
+/**
+ * Conversation
+ */
+
+export interface IConversationListenEvents {
+  "conversation:open": (receiverId: number) => void;
+  "conversation:close": (conversationId: number) => void;
+  "conversation:message:send": (message: Omit<IFullMessage, "id">) => void;
+}
+
+export interface IConversationEmitEvents {
   "conversation:message:received": (message: IFullMessage) => void;
   "conversation:load": (
     conversation: IConversation,
     messages: IFullMessage[]
   ) => void;
+}
+
+export type TConversationIO = Server<
+  IConversationListenEvents,
+  IConversationEmitEvents
+>;
+
+export type TConversationSocket = Socket<
+  IConversationListenEvents,
+  IConversationEmitEvents
+>;
+
+/**
+ * Room
+ */
+
+export interface IRoomListenEvents {
+  "room:message:send": (message: Omit<IFullMessage, "id">) => void;
+  "room:join": (roomId: number) => void;
+  "room:leave": (roomId: number) => void;
+}
+
+export interface IRoomEmitEvents {
   "room:message:received": (message: IFullMessage) => void;
   "room:load": (room: IRoom, messages: IFullMessage[]) => void;
 }
 
-export interface ListenEvents {
-  "conversation:open": (receiverId: number) => void;
-  "conversation:close": (conversationId: number) => void;
-  "conversation:message:send": (message: Omit<IFullMessage, "id">) => void;
-  "room:join": (roomId: number) => void;
-  "room:leave": (roomId: number) => void;
-  "room:message:send": (message: Omit<IFullMessage, "id">) => void;
-}
+export type TRoomIO = Server<IRoomListenEvents, IRoomEmitEvents>;
 
-export interface InterServerEvents {
-  ping: () => void;
-}
+export type TRoomSocket = Socket<IRoomListenEvents, IRoomEmitEvents>;
 
-export interface SocketData {
-  name: string;
-  age: number;
-}
+/**
+ * Socket
+ */
 
-export type Socket = IOSocket<
-  ListenEvents,
-  EmitEvents,
-  InterServerEvents,
-  SocketData
+export type TSocket = Socket<
+  IConversationListenEvents | IRoomListenEvents,
+  IConversationEmitEvents | IRoomEmitEvents
 >;
