befc8c653764d09f1d108f2e77bf7133f8a93c85
Arthur_gratton <arthur290799@gmail.com>
2022-12-30 23:58:36 +0100

    Add sass dependancies, fix dark theme feature and start login

---
+++ client/src/components/Login/login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+const redirectToPath = (path: string) => router.push(path);
+const redirectToRoute = (route: string) => router.push({ name: route });
+const showAlertQuery = () =>
+    alert(
+        route.query.test ?? "Type '?test=something' in the url to see the result"
+    );
+</script>
+
+<template>
+  <form action="">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="Password">
+    <input type="submit">
+  </form>
+</template>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -4,6 +4,7 @@ import ChatBox from "../components/ChatBox/ChatBox.vue";
 import ChatBot from "../components/ChatBot/ChatBot.vue";
 // import ChatRoom from "../components/ChatRoom/ChatRoom.vue";
 import NotFound from "../components/Errors/NotFound.vue";
+import Login from "../components/Login/login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: "/chatbot",
       name: "chatbot",
       component: ChatBot,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
     },
     /*
     {
